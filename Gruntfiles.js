a65654eaa9652caac317171f1e53ec36a8eab6f1
xiaoxuqin <2837303093@qq.com>
2020-04-08 12:39:59 +0800

    v0.5

--- Gruntfiles.js
+++ Gruntfiles.js
@@ -1,29 +1,83 @@
 /**global module:true */
-module.exports = function (grunt) {  
+module.exports = function (grunt) {
     grunt.initConfig({
-        eslint:{
-            options:{
-                configFile:'.eslintrc.json'
+        eslint: {
+            options: {
+                configFile: '.eslintrc.json'
             },
-            target:['*.js']
+            target: ['*.js']
         },
-        csslint:{
-            options:{
-                csslintrc:'.csslintrc'
+        csslint: {
+            options: {
+                csslintrc: '.csslintrc'
             },
-            src:'*.css'
+            src: '*.css'
         },
-        htmlhint:{
-            options:{
-                htmlhintrc:'.htmlhintrc'
+        htmlhint: {
+            options: {
+                htmlhintrc: '.htmlhintrc'
             },
-            src:'*.html'
+            src: '*.html'
+        },
+        htmlmin: {
+            options: {
+                collapseWhitespace: true,
+                preserveLineBreaks: false
+            },
+            files: {
+                src: 'dist/index.html',
+                dest: 'dist/index.html'
+            }
+        },
+        cssmin: {
+            'dist/rectangle.css': 'rectangle.css'
+        },
+        uglify: {
+            release: {
+                files: {
+                    'dist/bundle.min.js': 'dist/bundle.js',
+                }
+            }
+        },
+        useminPrepare: {
+            html: 'index.html',
+            options: {
+                dest: 'dist'
+            }
+        },
+        usemin: {
+            html: ['dist/index.html']
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            js: {
+                src: ['rectangle.js', 'calc.js'],
+                dest: 'dist/bundle.js'
+            }
+        },
+        clean: ['dist/bundle.js', '.tmp'],
+        copy: {
+            html: {
+                src: './index.html',
+                dest: './dist/index.html'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-csslint');
     grunt.loadNpmTasks('grunt-htmlhint');
     grunt.loadNpmTasks('grunt-eslint');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+  
 
     grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
+    grunt.registerTask('release', ['copy', 'useminPrepare', 'concat', 'uglify', 'usemin', 'cssmin', 'htmlmin', 'clean']);
 };
